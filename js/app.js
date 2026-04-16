101267203a3845061a8a2bebf9a5fcb2af0f51b7
kyw04 <85551017+kyw04@users.noreply.github.com>
2026-04-16 15:31:08 +0900

    feat: add skills list frame

--- js/app.js
+++ js/app.js
@@ -1,11 +1,40 @@
 function CopyEmail()
 {
-  const email = document.getElementById("email").innerText;
-  navigator.clipboard.writeText(email).then(() =>
-  {
-    alert("이메일이 복사되었습니다!");
-  }).catch(() =>
-  {
-    alert("복사 실패");
-  });
+    const email = document.getElementById("email").innerText;
+    navigator.clipboard.writeText(email).then(() =>
+    {
+        alert("이메일이 복사되었습니다!");
+    }).catch(() =>
+    {
+        alert("복사 실패");
+    });
 }
+
+
+document.addEventListener('DOMContentLoaded', () =>
+{
+    const parents = document.querySelectorAll('.skill');
+
+    parents.forEach(parent =>
+    {
+        parent.addEventListener('click', function (event)
+        {
+            const explanation = parent.querySelector('.tail');
+            const img = parent.querySelector('.pin');
+
+            if (explanation && img)
+            {
+                if (parent.querySelector('.fixed'))
+                {
+                    explanation.classList.remove('fixed');
+                    img.classList.remove('fixed');
+                }
+                else
+                {
+                    explanation.classList.add('fixed');
+                    img.classList.add('fixed');
+                }
+            }
+        });
+    });
+});
